83d0399933a669145a9fd8bc2337a776d5ab95f2
Netflixy <netflixycontact@gmail.com>
2024-05-08 18:29:48 +0200

    feat: add web launcher

---
+++ launcher/assets/js/facebook.js
@@ -0,0 +1,30 @@
+window.fbAsyncInit = function () {
+    // Initialize the SDK with your app and the Graph API version for your app 
+    FB.init({
+        appId: '357140426152444',
+        xfbml: true,
+        version: 'v19.0'
+    });
+};
+
+// Add the Facebook SDK for Javascript
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/fr_FR/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk')
+);
+
+function isFBConnected() {
+    return new Promise((resolve, reject) => {
+        FB.getLoginStatus(function (response) {
+            if (response.status === 'connected') {
+                resolve(response.authResponse.accessToken)
+            } else {
+                resolve(false)
+            }
+        });
+    })
+}
